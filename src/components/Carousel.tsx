769edc239c8ecbc0a5243d8271aa80fdca060bc6
the1crows <113790120+the1crows@users.noreply.github.com>
2024-02-25 11:13:15 +0100

    Create Carousel.tsx

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { WelcomeBanner } from '../sections/Dashboard/WelcomeBanner';
+import { TokenListingBanner } from '../sections/Dashboard/NewBanner';
+
+export function NewsCarousel() {
+  const banners = [
+    <WelcomeBanner key={1} />
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: banners.length > 1,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
+  return (
+    <Slider {...settings}>
+      {banners.map((banner, index) => (
+        <div key={index}>
+          {banner}
+        </div>
+      ))}
+    </Slider>
+  );
+}
